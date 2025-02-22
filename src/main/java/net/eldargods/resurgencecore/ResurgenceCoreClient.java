21e8f57b5565776d4e63911201640ef4236ed77c
EldarGods <ryan.diaj@gmail.com>
2025-02-22 16:20:24 +0000

    Initial fix

---
+++ src/main/java/net/eldargods/resurgencecore/ResurgenceCoreClient.java
@@ -0,0 +1,10 @@
+package net.eldargods.resurgencecore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResurgenceCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/eldargods/resurgencecore/ResurgenceCoreDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eldargods.resurgencecore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ResurgenceCoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
